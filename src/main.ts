d0369a39f70f864d85cd2f693363773c30e908b4
manhhuy140805 <trandinhmanhhuy05@gmail.com>
2026-03-25 23:52:21 +0700

    cấp quyền cho FE

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://book-nest-fe-react-js.vercel.app',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
